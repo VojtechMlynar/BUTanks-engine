b536bb0e5c2e3502350305e77c7c54525b84bda6
Daniel Boháč <44163093+Kophejn@users.noreply.github.com>
2021-06-09 22:41:03 +0200

    Update engine.py
    FPS independence (basic) and initial style corrections.

--- code/engine/engine.py
+++ code/engine/engine.py
@@ -1,26 +1,32 @@
-# BUTanks engine v0
+""" BUTanks engine v0.0.1 - dev_DB branch
+
+Main goal: FPS independence and style corrections (not final)
+""" 
 
 import os
-from numpy.core.fromnumeric import size
+import math
+import time
 import pygame
 import numpy as np
-import math
 from pathlib import Path
 
+# CONSTANTS
 WIDTH, HEIGHT = 1000, 1000
 WHITE = (255,255,255)
-FPS = 60
+TARGET_FPS = 60
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("BUTanks engine")
-
+# PATHS
 p = Path(__file__).parents[1]
 ASSETS_DIR = os.path.join(p,"assets")
 MAPS_DIR = os.path.join(ASSETS_DIR,"maps")
 IMGS_DIR = os.path.join(ASSETS_DIR,"images")
 
+
 class Ball(pygame.sprite.Sprite):
+    """Ball class docstring."""
+
     def __init__(self, pos_x, pos_y, v0, phi0, f, img_path):
+        """Class method docstring."""
         super(Ball, self).__init__()
         self.x = pos_x
         self.y = pos_y
@@ -33,10 +39,11 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
 
-    def update(self, environment):
-        self.v += -1*np.sign(self.v)*(self.f)/FPS
-        self.x += self.v*math.sin(self.phi)/FPS
-        self.y += self.v*math.cos(self.phi)/FPS
+    def update(self, environment, dt):
+        """Class method docstring."""
+        self.v += -1*np.sign(self.v)*(self.f) *dt
+        self.x += self.v*math.sin(self.phi) *dt
+        self.y += self.v*math.cos(self.phi) *dt
         self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
 
         col= pygame.sprite.collide_mask(self,environment)
@@ -44,10 +51,11 @@ class Ball(pygame.sprite.Sprite):
             self.v = -1*self.v
             print("boink :",col)
 
-
-
 class Background(pygame.sprite.Sprite):
+    """Ball class docstring."""
+    
     def __init__(self, img_path):
+        """Class method docstring."""
         super(Background, self).__init__()
         self.x = 0
         self.y = 0
@@ -59,14 +67,23 @@ class Background(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
+        """Class method docstring."""
         pass        
 
+
 def draw_window():
-    
+    """Function docstring."""
+
     #WIN.blit(SPRITE,(300,100))
     pygame.display.update()
 
+# MAIN LOOP
 def main():
+    fps = TARGET_FPS
+    prev_time = time.time()
+
+    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("BUTanks engine")
     clock = pygame.time.Clock()
     ball = Ball(200, 200, 40, 0.1, 0, "ball.png")
     ballsprite = pygame.sprite.RenderPlain(ball)
@@ -74,15 +91,23 @@ def main():
     arenasprite = pygame.sprite.RenderPlain(arena)
 
     run = True
+
+    # GAME LOOP
     while run:
-        clock.tick(FPS)
+        clock.tick(TARGET_FPS)  # Limit framerate
+        
+        # Compute delta time
+        now = time.time()
+        dt = now - prev_time
+        prev_time = now
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         WIN.fill(WHITE)
         #ball.update()
               
-        ball.update(arena)
+        ball.update(arena, dt)
         arenasprite.draw(WIN)    
         ballsprite.draw(WIN)
         
